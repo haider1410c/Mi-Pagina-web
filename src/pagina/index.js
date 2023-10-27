5944cc369a09dd0bb16a2d05562cbb22137869a1
Mercadeo <publicidadfrescas@gmail.com>
2023-10-27 16:49:42 -0500

    cambio

--- src/pagina/index.js
+++ src/pagina/index.js
@@ -1,6 +1,19 @@
 import Typed from 'typed.js';
 
 const typed = new Typed('.typed', {
-    String: ['Hola, soy desarrollador web']
+    String: ['Hola, soy desarrollador web'],
+
+
+    typeSpeed: 75,
+    startDelay: 300,
+    backSpeed: 75,
+    backDelay: 1000,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '|',
+    attr: null,
+    contentType: 'html',
+
 });
 
