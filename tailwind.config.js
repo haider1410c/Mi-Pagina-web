0f2453162e43e7069ef8b28a37b1aa158c624ad0
Mercadeo <publicidadfrescas@gmail.com>
2023-10-30 16:44:21 -0500

    cambios

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,23 @@ module.exports = {
       'xl': '1366px',
       '2xl': '1536px',
     },
+    fontSize: {
+      'xs': '.75rem',     // 12px
+      'sm': '.875rem',    // 14px
+      'tiny': '.875rem',  // 14px
+      'base': '1rem',     // 16px
+      "base2":"1.1rem",   // 17.6px
+      'lg': '1.125rem',   // 18px
+      'xl': '1.25rem',    // 20px
+      '2xl': '1.5rem',    // 24px
+      '3xl': '1.875rem',  // 30px
+      '4xl': '2.25rem',   // 36px
+      '5xl': '3rem',      // 48px
+      '6xl': '4rem',      // 64px
+      '7xl': '5rem',      // 80px
+      '8xl': '6rem',      // 96px
+      '9xl': '8rem',      // 128px
+    },
 
     extend: {
 
